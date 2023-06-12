fff9657d9ddbe3d82efda1bb215cfa9521c97796
SpectrumByte <paola121103@gmail.com>
2023-06-12 14:44:18 -0600

    Added Interface program

---
+++ src/InterfacesProgram1/Animal.java
@@ -0,0 +1,12 @@
+package InterfacesProgram1;
+/*
+Interface Animal implemented by MammalInt class.
+It is like a template of methods the class have to use.
+
+@author Anette Larios
+@since 12.06.2023
+ */
+interface Animal{
+     public void eat();
+     public void travel();
+ }
